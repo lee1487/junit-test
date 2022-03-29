f10b198a9714a00863d39a178538da0f3a6d6233
이현세 <dlgustp1487@naver.com>
2022-03-29 14:19:42 +0900

    Junit5 시작하기

---
+++ src/main/java/hello/infleranthetest/InflearnTheTestApplication.java
@@ -0,0 +1,13 @@
+package hello.infleranthetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnTheTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnTheTestApplication.class, args);
+	}
+
+}

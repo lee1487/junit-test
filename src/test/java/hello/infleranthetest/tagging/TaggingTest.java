11de2aa0e057cbd10e0696c0e17520cd99938ab4
이현세 <dlgustp1487@naver.com>
2022-03-29 16:58:31 +0900

    JUnit5 커스텀 태그

--- src/test/java/hello/infleranthetest/tagging/TaggingTest.java
+++ src/test/java/hello/infleranthetest/tagging/TaggingTest.java
@@ -9,10 +9,14 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import hello.infleranthetest.Study;
+import hello.infleranthetest.annotation.FastTest;
+import hello.infleranthetest.annotation.SlowTest;
 
 class TaggingTest {
 
-
+	//테스트 파일 - run configuration - include and exclude tags Configuration 설정으로 fast추가하면 fast만 실행
+	//maven test -> pom.xml profiles 태그 참조, 프로젝트 우클릭 Maven Select maven profiles 에서 id 선택후 maven test 실행
+	
 	@Test
 	@DisplayName("태깅 테스트 fast")
 	@Tag("fast")
@@ -27,6 +31,20 @@ class TaggingTest {
 	@Tag("slow")
 	void tagging2() {
 		System.out.println("tagging slow");
+	}
+	
+	@FastTest
+	@DisplayName("태깅 테스트 fast")
+	void tagging3() {
+		System.out.println("custom tagging fast");
+		Study study = new Study(100);
+		assertThat(study.getLimit()).isGreaterThan(0);
+	}
+	
+	@SlowTest
+	@DisplayName("태깅 테스트 slow")
+	void tagging4() {
+		System.out.println("custom tagging slow");
 	}
 
 }
